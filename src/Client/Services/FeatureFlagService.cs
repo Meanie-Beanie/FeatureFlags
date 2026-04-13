677773f69f04a9243a006f88e21b88a5043b650d
Hirvonen Olli <2506134@edu.karelia.fi>
2026-04-13 03:11:02 +0300

    Setting up test cases for Flag Service

--- src/Client/Services/FeatureFlagService.cs
+++ src/Client/Services/FeatureFlagService.cs
@@ -5,6 +5,14 @@ namespace Client.Services;
 
 public class FeatureFlagService : IFeatureFlagService
 {
+
+    private readonly HttpClient _client = new();
+
+    public FeatureFlagService(HttpClient client)
+    {
+        _client = client;
+    }
+
     public Task<AuthResponse> GetFeatureFlags(string apiKey)
     {
         throw new NotImplementedException();
---
+++ tests/unit/Client.UnitTests/Services/FeatureFlagServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Client.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.UnitTests.Services;
+public class FeatureFlagServiceTests
+{
+    [Fact]
+    public void GetFeatureFlags_GetsValidAPIResponse_ReturnsAuthResponse()
+    {
+        var testApiKey = "testuri";
+
+        var httpClientMock = new Mock<HttpClient>();
+        httpClientMock.Setup(x => x.GetAsync(testApiKey));
+
+        var featureFlagService = new FeatureFlagService(httpClientMock.Object);
+        var result = featureFlagService.GetFeatureFlags(testApiKey);
+    }
+}
