7431c6ff4d2e1416bfcf570b9e343e029a6a034a
Hirvonen Olli <2506134@edu.karelia.fi>
2026-04-16 02:01:06 +0300

    Adding client side constants for API usage (like headers) and expanding featureflagservice

--- src/Client/Services/FeatureFlagService.cs
+++ src/Client/Services/FeatureFlagService.cs
@@ -1,11 +1,14 @@
 ﻿using Client.Interfaces;
 using Shared.Responses;
+using Shared.Routes;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Client.Services;
 
 public class FeatureFlagService : IFeatureFlagService
 {
-
     private readonly HttpClient _client = new();
 
     public FeatureFlagService(HttpClient client)
@@ -13,8 +16,18 @@ public class FeatureFlagService : IFeatureFlagService
         _client = client;
     }
 
-    public Task<AuthResponse> GetFeatureFlags(string apiKey)
+    public async Task<AuthResponse> GetFeatureFlags(string apiKey)
     {
-        throw new NotImplementedException();
+        // We are using one HttpClient without DI (singleton) so we do not change the base properties of the httpclient
+        // therefore we create HttpRequestMessage -class to wrap our specific needs and send it instead.
+        HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, ApiRoutes.Features.Base);
+        requestMessage.Headers.Add(Constants.Api.ApiKeyHeader, apiKey);
+
+        var response = await _client.SendAsync(requestMessage);
+
+        var content = await response.Content.ReadAsStringAsync();
+        var authResponse = JsonSerializer.Deserialize<AuthResponse>(content);
+
+        return authResponse;
     }
 }
--- src/Shared/Routes/ApiRoutes.cs
+++ src/Shared/Routes/ApiRoutes.cs
@@ -6,22 +6,25 @@ using System.Threading.Tasks;
 
 namespace Shared.Routes;
 
+// Routing file for API routes.
+
 /*
-Routing file for API routes.
-
-Note:
-
 We will have attribute routing for API routes so we will use things like:
 "/{id}" as pure string as the Attribute routing will handle 'injecting' the routing parameter. It is also not capitalized as parameters are not.
 
 Read more: https://learn.microsoft.com/en-us/aspnet/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
+
+** Important**
+
+These routes are for our upcoming API project. Later on, we'll have to create a client version that can take these same paths but adjust them accordingly.
+Reasoning is that httpclient will use connection strings as parameter but attribute binding requires differently built strings.
 */
 public static class ApiRoutes
 {
     public static class Features
     {
         public const string Base = "api/features";
-        // public static string GetFeature = Base + "/{Id]";
+        // public static string GetFeature = Base + "/{id]";
 
     }
 }
