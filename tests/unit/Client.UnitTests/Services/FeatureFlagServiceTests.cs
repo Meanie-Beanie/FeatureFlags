2ccfadb71aab0a536cc1ecb3c5cd81d3df21913f
Hirvonen Olli <2506134@edu.karelia.fi>
2026-04-21 17:28:17 +0300

    Adding test that tests for empty,null or whitespaced api key

--- tests/unit/Client.UnitTests/Services/FeatureFlagServiceTests.cs
+++ tests/unit/Client.UnitTests/Services/FeatureFlagServiceTests.cs
@@ -15,7 +15,7 @@ namespace Client.UnitTests.Services;
 public class FeatureFlagServiceTests
 {
     [Fact]
-    public async Task GetFeatureFlags_APIReturnsWithAuthorizedFeatures_ReturnsAuthResponse()
+    public async Task GetFeatureFlags_ApiReturnsWithAuthorizedFeatures_ReturnsAuthResponse()
     {
         var baseUrl = new Uri("https://www.test.com");
         var featureUrl = baseUrl + ApiRoutes.Features.Base;
@@ -42,5 +42,32 @@ public class FeatureFlagServiceTests
         Assert.True(string.IsNullOrEmpty(result.ErrorMessage));
         Assert.Equal(features.Count, result.Features.Count);
         Assert.Equal(features.Select(x => x.Name), result.Features.Select(x => x.Name));
+    }
+
+    [Theory]
+    [InlineData(null)] // Null
+    [InlineData("")] // Empty
+    [InlineData("              ")] //Whitespaces
+    public async Task GetFeatureFlags_EmptyApiKeyProvided_ThrowsArgumentException(string apiKey)
+    {
+        var baseUrl = new Uri("https://www.test.com");
+        var featureUrl = baseUrl + ApiRoutes.Features.Base;
+
+        var features = FeatureFlagBuilder.CreateFeatures();
+        var authResponseJson = AuthResponseBuilder.CreateJsonAuthResponse(true, HttpStatusCode.OK, null, features);
+
+        var mockHttp = new MockHttpMessageHandler();
+
+        mockHttp.Expect(HttpMethod.Get, featureUrl)
+                .WithHeaders(Constants.Api.ApiKeyHeader, apiKey)
+                .Respond("application/json", authResponseJson); // Respond with JSON
+
+        // Inject the handler or client into your application code
+        var client = mockHttp.ToHttpClient();
+        client.BaseAddress = baseUrl;
+
+        var sut = new FeatureFlagService(client);
+
+        await Assert.ThrowsAsync<ArgumentException>(() => sut.GetFeatureFlags(apiKey));
     }
 }
