46fde43d448e7b7adb5c9a2307294275705120e6
Hirvonen Olli <2506134@edu.karelia.fi>
2026-04-28 03:42:14 +0300

    Negative tests for FeedbackFeatures

--- tests/unit/Client.UnitTests/Features/FeedbackFeatureTests.cs
+++ tests/unit/Client.UnitTests/Features/FeedbackFeatureTests.cs
@@ -24,4 +24,20 @@ public class FeedbackFeatureTests
 
         Assert.True(sut.CanUse);
     }
+
+    // Both properties need to be true for the feature to be activated, so we test the cases where one of them or both are not.
+    [Theory]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    [InlineData(false, false)]
+    public void CanUse_FeatureIsDisabled_ReturnsFalse(bool isAuthorized, bool HasFeature)
+    {
+        var featureAccessMock = new Mock<IFeatureAccessService>();
+        featureAccessMock.Setup(x => x.IsUserAuthorized).Returns(isAuthorized);
+        featureAccessMock.Setup(x => x.HasFeature(It.IsAny<string>())).Returns(HasFeature);
+
+        var sut = new FeedbackFeature(featureAccessMock.Object);
+
+        Assert.False(sut.CanUse);
+    }
 }
