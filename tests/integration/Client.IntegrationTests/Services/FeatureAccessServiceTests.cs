bd2525421929101b5f09c2074c4cff89ba365863
Hirvonen Olli <2506134@edu.karelia.fi>
2026-07-25 03:54:51 +0300

    Starting integrationTests, relocating testutils into a separate folder so they can be used by IntegrationTests.

---
+++ tests/integration/Client.IntegrationTests/Services/FeatureAccessServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Client.IntegrationTests.Services;
+
+
+public class FeatureAccessServiceTests
+{
+
+}
